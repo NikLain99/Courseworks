6fdf702b8f6fe1b5a5540a167e49837674fa241b
NikLain99 <nkutsakivskyi@gmail.com>
2026-03-17 21:56:13 +0100

    added hometask

---
+++ sql/hometasks/query.sql
@@ -0,0 +1,67 @@
+-----------------Zadanie-1-----------------
+
+SELECT 
+    w.data_wyp,
+    k.imie_klienta,
+    k.nazwisko_klienta
+FROM
+    wypozyczenia w
+    LEFT JOIN klienci k USING (id_klienta)
+ORDER BY
+    w.data_wyp,
+    k.imie_klienta,
+    k.nazwisko_klienta;
+
+-----------------Zadanie-2-----------------
+
+SELECT DISTINCT
+    s.marka,
+    s.model,
+    dw.cena_doba
+FROM
+    samochody s
+    JOIN dane_wypozyczen dw USING (id_samochodu)
+ORDER BY
+    dw.cena_doba DESC
+
+-----------------Zadanie-3-----------------
+
+SELECT DISTINCT
+    p.id_pracownika,
+    p.imie_pracownika,
+    p.nazwisko_pracownika
+FROM
+    pracownicy p
+    JOIN wypozyczenia w USING (id_pracownika)
+WHERE
+    YEAR(w.data_wyp) = 2022 and MONTH(w.data_wyp) = 7
+
+-----------------Zadanie-4-----------------
+
+SELECT DISTINCT
+    k.nazwisko_klienta,
+    s.marka,
+    s.model
+FROM
+    samochody s
+    JOIN dane_wypozyczen dw USING (id_samochodu)
+    JOIN wypozyczenia w USING (id_wypozyczenia)
+    JOIN klienci k USING (id_klienta)
+WHERE
+    k.nazwisko_klienta = "Karwowski"
+    AND MONTH(w.data_wyp) BETWEEN 4 AND 9
+
+-----------------Zadanie-5-----------------
+
+SELECT
+    p.nazwisko_pracownika,
+    dw.cena_doba*dw.ilosc_dob as wartość_wypożyczenia,
+    w.data_wyp
+FROM
+    dane_wypozyczen dw
+    JOIN wypozyczenia w USING (id_wypozyczenia)
+    JOIN pracownicy p USING (id_pracownika)
+WHERE
+    p.nazwisko_pracownika = "Nowacki"
+ORDER BY
+    w.data_wyp
